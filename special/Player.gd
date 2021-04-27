b88cb74d47108a6d2865691f6cfab0acd730ca71
ollintlaloc@gmail.com <46585763+rullinoiz@users.noreply.github.com>
2021-04-27 10:59:52 -0700

    new stuff

---
+++ special/Player.gd
@@ -0,0 +1,32 @@
+extends KinematicBody2D
+
+const MOVESPEED = 500
+
+onready var root = get_tree().get_root()
+
+var health = 100
+
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	
+	print(delta)
+	
+	var motion = Vector2()
+	
+	# check if button pressed
+	if Input.is_action_pressed("player_up"):
+		motion.y -= 1
+	if Input.is_action_pressed("player_down"):
+		motion.y += 1
+	if Input.is_action_pressed("player_left"):
+		motion.x -= 1
+	if Input.is_action_pressed("player_right"):
+		motion.x += 1
+	
+	# now apply that to the player
+	motion = motion.normalized()                 # make it so you don't go faster when diagnal
+	motion = move_and_slide(motion * MOVESPEED) # make the player collidable
+	look_at(get_global_mouse_position())        # as with most top-down shooters, you should be able to aim
+	
