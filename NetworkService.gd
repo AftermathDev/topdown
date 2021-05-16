b9cf3ea23a7fad4b0cb208cbb2ccaa426e188dff
ollintlaloc@gmail.com <46585763+rullinoiz@users.noreply.github.com>
2021-05-15 17:24:53 -0700

    writing client :flushed:

---
+++ NetworkService.gd
@@ -0,0 +1,16 @@
+extends Node
+
+const IP_ADDR = "73.252.145.40"
+const PORT    = 25454
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
